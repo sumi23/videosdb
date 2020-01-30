aeaa4fdd139de68d0f32161ad7687bc880bebb81
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-01-30 03:58:05 -0500

    Update reference_urls.sql

--- reference_urls.sql
+++ reference_urls.sql
@@ -4,7 +4,7 @@ name varchar(20) not null,
 url varchar(20) not null,
 description text,
 created_by varchar(20) not null,
-created_on timestamp not null,
+created_on timestamp not null default current_timestamp,
 modified_by varchar(20),
-modified_on timestamp);
+modified_on timestamp default current_timestamp);
 
