d3a0a101e81908bee007cfc1f093c41eab67f02b
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-01-30 03:58:28 -0500

    Update sample_programs.sql

--- sample_programs.sql
+++ sample_programs.sql
@@ -4,6 +4,6 @@ name varchar(20) not null,
 file longblob not null,
 description text,
 created_by varchar(20) not null,
-created_on timestamp not null,
+created_on timestamp not null default current_timestamp,
 modified_by varchar(20),
-modified_on timestamp);
+modified_on timestamp default current_timestamp);
