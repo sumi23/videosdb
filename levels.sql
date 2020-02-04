9ea13eda90f48c5c48e080231db538acd58b082d
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 05:13:17 -0500

    Delete levels.sql

--- levels.sql
+++
@@ -1,9 +0,0 @@
-create table levels(
-id int primary key,
-name varchar(10) not null,
-prerequisite varchar(10),
-created_by varchar(20) not null,
-created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
-modified_on timestamp default current_timestamp
-);
