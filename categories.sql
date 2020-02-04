ce38da37bab4b53fd35fd8f9f5972a16b77281af
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 05:12:05 -0500

    Delete categories.sql

--- categories.sql
+++
@@ -1,9 +0,0 @@
-create table categories(
-id int primary key,
-name varchar(50) not null,
-status boolean,
-created_by varchar(20) not null,
-created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
-modified_on timestamp default current_timestamp
-);
