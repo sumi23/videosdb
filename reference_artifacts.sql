4769384f70124de61861dd2d0f08558ec53ad2d6
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 05:13:48 -0500

    Delete reference_artifacts.sql

--- reference_artifacts.sql
+++
@@ -1,9 +0,0 @@
-create table reference_artifact(
-id int primary key,
-name varchar(20) not null,
-file longblob,
-description text,
-created_by varchar(20) not null,
-created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
-modified_on timestamp default current_timestamp);
