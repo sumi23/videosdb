0274bbe188bd166e6ce021664e55c8a39f88ccdf
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 01:12:00 -0500

    Update reference_artifacts.sql

--- reference_artifacts.sql
+++ reference_artifacts.sql
@@ -1,6 +1,7 @@
-create table reference_artifact(
+create table reference_artifacts(
 id int primary key,
 name varchar(20) not null,
+constraint unique_name unique(name),
 file longblob,
 description text,
 created_by varchar(20) not null,
