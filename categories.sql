a5a08cf4eb0d9f1d67f9e95efb11e79e98114860
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 04:21:30 -0500

    Update categories.sql

--- categories.sql
+++ categories.sql
@@ -3,8 +3,8 @@ id int primary key,
 name varchar(50) not null,
 constraint unique_name unique(name),
 status boolean,
-created_by varchar(20) not null ,
+created_by varchar(30) not null ,
 created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
+modified_by varchar(30),
 modified_on timestamp default current_timestamp
 );
