569653819a3cf709225aa6cf03b4d33da13c887b
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 04:23:23 -0500

    Update sample_programs.sql

--- sample_programs.sql
+++ sample_programs.sql
@@ -1,12 +1,12 @@
 create table sample_programs(
 id int primary key,
-name varchar(20) not null,
+name varchar(30) not null,
 constraint unique_name unique(name),
 file longblob not null,
 description text,
-created_by varchar(20) not null,
+created_by varchar(30) not null,
 created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
+modified_by varchar(30),
 modified_on timestamp default current_timestamp,
 video_id int not null,
 constraint fk_sample_programs_video_id foreign key(video_id) references videos(id));
