356514cab0027a5727c6ee05496eaa8d750fd25e
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 04:22:41 -0500

    Update reference_urls.sql

--- reference_urls.sql
+++ reference_urls.sql
@@ -1,12 +1,12 @@
 create table reference_urls(
 id int primary key,
-name varchar(20) not null,
+name varchar(30) not null,
 constraint unique_name unique(name),
 url varchar(50) not null,
 description text,
-created_by varchar(20) not null,
+created_by varchar(30) not null,
 created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
+modified_by varchar(30),
 modified_on timestamp default current_timestamp,
 video_id int not null,
 constraint fk_reference_urls_video_id foreign key(video_id) references videos(id));
