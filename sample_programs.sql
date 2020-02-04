0653e20240b023e9540647419787dcba510f9345
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 01:29:02 -0500

    Update sample_programs.sql

--- sample_programs.sql
+++ sample_programs.sql
@@ -7,4 +7,6 @@ description text,
 created_by varchar(20) not null,
 created_on timestamp not null default current_timestamp,
 modified_by varchar(20),
-modified_on timestamp default current_timestamp);
+modified_on timestamp default current_timestamp,
+video_id int not null,
+constraint fk_sample_programs_video_id foreign key(video_id) references videos(id));
