3f07a63ece0b779f02efe06f584d399f0c167e2f
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-04 02:33:55 -0500

    Update reference_urls.sql

--- reference_urls.sql
+++ reference_urls.sql
@@ -2,7 +2,7 @@ create table reference_urls(
 id int primary key,
 name varchar(20) not null,
 constraint unique_name unique(name),
-url varchar(20) not null,
+url varchar(50) not null,
 description text,
 created_by varchar(20) not null,
 created_on timestamp not null default current_timestamp,
