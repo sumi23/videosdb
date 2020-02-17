4085ff0dca7c6db24b2bf5f17c21104c0200e76c
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-17 09:18:55 -0500

    Update sample_programs.sql

--- sample_programs.sql
+++ sample_programs.sql
@@ -1,5 +1,5 @@
 create table sample_programs(
-id int primary key,
+id int primary key auto_increment,
 name varchar(30) not null,
 constraint unique_sample_program_name unique(name),
 file longblob not null,
