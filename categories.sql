ad614946908a20339b02c04812f57fee3af12090
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-19 23:52:35 -0500

    Update categories.sql

--- categories.sql
+++ categories.sql
@@ -2,9 +2,4 @@ create table categories(
 id int primary key auto_increment,
 name varchar(50) not null,
 constraint unique_category_name unique(name),
-status boolean,
-created_by varchar(30) not null ,
-created_on timestamp not null default current_timestamp,
-modified_by varchar(30),
-modified_on timestamp default current_timestamp
 );
