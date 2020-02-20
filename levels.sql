3c6ad1fce4413dfc464cfe7200fd5577800667a3
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-19 23:52:13 -0500

    Update levels.sql

--- levels.sql
+++ levels.sql
@@ -2,9 +2,4 @@ create table levels(
 id int primary key auto_increment,
 name varchar(10) not null,
 constraint unique_level_name unique(name),
-prerequisite varchar(10),
-created_by varchar(20) not null ,
-created_on timestamp not null default current_timestamp,
-modified_by varchar(20),
-modified_on timestamp default current_timestamp
 );
