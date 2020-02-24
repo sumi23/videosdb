450c35a94e11c2e06425b58267e6a834f31496b3
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-02-24 08:02:56 -0500

    Update reference_urls.sql

--- reference_urls.sql
+++ reference_urls.sql
@@ -5,5 +5,5 @@ constraint unique_reference_url_name unique(name),
 url varchar(50) not null,
 description text,
 video_id int not null,
-constraint fk_reference_urls_video_id foreign key(video_id) references videos(id));
+constraint fk_reference_urls_video_id foreign key(video_id) references videos(id) on delete cascade);
 
