5025ba960bb665618543803e2bf6d58f29ccdccd
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-03-23 16:05:49 +0530

    Update reference_artifacts.sql

--- reference_artifacts.sql
+++ reference_artifacts.sql
@@ -1,8 +1,8 @@
 create table reference_artifacts(
 id int primary key auto_increment,
-name varchar(30) not null,
+name varchar(50) not null,
 constraint unique_reference_artifact_name unique(name),
-file longblob not null,
-description text,
+file varchar(50) not null,
+description varchar(200),
 video_id int not null,
 constraint fk_reference_artifacts_video_id foreign key(video_id) references videos(id) on delete cascade);
