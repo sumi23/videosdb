6ced7199fa0f732833859a0438c529eae89385c3
sumi23 <52774752+sumi23@users.noreply.github.com>
2020-03-23 16:07:16 +0530

    Update reference_urls.sql

--- reference_urls.sql
+++ reference_urls.sql
@@ -1,9 +1,9 @@
 create table reference_urls(
 id int primary key auto_increment,
-name varchar(30) not null,
+name varchar(50) not null,
 constraint unique_reference_url_name unique(name),
 url varchar(50) not null,
-description text,
+description varchar(200),
 video_id int not null,
 constraint fk_reference_urls_video_id foreign key(video_id) references videos(id) on delete cascade);
 
